b23175a0d920fe4a9c985dc738e76460060ce2b4
simjsik <simjsik75@naver.com>
2024-12-24 03:58:13 +0900

    엔드포인트 위치 조정

--- index.js
+++ index.js
@@ -26,21 +26,21 @@ app.get("/", (req, res) => {
     res.status(200).send("WebSocket 서버가 실행 중입니다.");
 });
 
+// HTTP POST 요청을 처리하는 엔드포인트 추가
+app.post("/notice", (req, res) => {
+    const { postId, title, message } = req.body;
+
+    console.log("알림 수신 POST 요청:", { postId, title, message });
+
+    // WebSocket을 통해 클라이언트로 알림 브로드캐스트
+    io.emit("new-notice", { postId, title, message });
+
+    res.status(200).send("알림 전송 성공");
+});
+
 // WebSocket 연결 이벤트
 io.on('connection', (socket) => {
     console.log('클라이언트 연결 성공');
-
-    // HTTP POST 요청을 처리하는 엔드포인트 추가
-    app.post("/notice", (req, res) => {
-        const { postId, title, message } = req.body;
-
-        console.log("알림 수신 POST 요청:", { postId, title, message });
-
-        // WebSocket을 통해 클라이언트로 알림 브로드캐스트
-        io.emit("new-notice", { postId, title, message });
-
-        res.status(200).send("알림 전송 성공");
-    });
 
     // 클라이언트 연결 해제 이벤트
     socket.on('disconnect', () => {
